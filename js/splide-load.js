bd4b09c1f9fff64e248cbf0a29c3bb1b9437789e
N4iri <ripvv0105@gmail.com>
2024-06-26 10:22:34 +0900

    update

---
+++ js/splide-load.js
@@ -0,0 +1,24 @@
+var splide = new Splide( '.splide',{
+    type: 'loop',
+    speed: 2000,
+    arrows: false,
+    pagination: false,
+    isNavigation: true,
+    // autoplay: true,
+    resetProgress: false,
+    // interval: 4000,
+    direction: 'ttb',
+    wheelSleep: 1000,
+    height:'100vh' ,
+} )
+splide.on( 'pagination:mounted', function ( data ) {
+    // ULエレメントにクラスを追加できます
+    data.list.classList.add( 'splide__pagination--custom' );
+  
+    // `items`は、すべてのLI要素とボタン要素を保持しています
+    data.items.forEach( function ( item ) {
+      item.button.textContent = String( item.page + 1 );
+    } );
+  } );
+
+splide.mount();
